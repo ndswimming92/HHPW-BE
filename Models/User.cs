741943b9e36eb0468715cef525abe6ec25ff68ce
Nicholas Davidson <nicholas.davidson.20@gmail.com>
2024-04-08 20:53:03 -0500

    Created User model

--- Models/User.cs
+++ Models/User.cs
@@ -1,6 +1,12 @@
-﻿namespace HHPW_BE.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HHPW_BE.Models
 {
     public class User
     {
+        public int Id { get; set; }
+        [Required]
+        public bool Uid { get; set; }
+        public string? Name { get; set; }
     }
 }
