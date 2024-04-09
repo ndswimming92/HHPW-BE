68e01e0e7a4abd67b608096fcad1ac8517ef77d6
Nicholas Davidson <nicholas.davidson.20@gmail.com>
2024-04-08 21:27:36 -0500

    OrdersData

--- Data/OrdersData.cs
+++ Data/OrdersData.cs
@@ -1,6 +1,10 @@
-﻿namespace HHPW_BE.Data
+﻿using HHPW_BE.Models;
+
+namespace HHPW_BE.Data
 {
     public class OrdersData
     {
+        public static List<Order> Orders = new List<Order>()
+        { };
     }
 }
