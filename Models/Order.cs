a4aa352703389fa9f07f62f3c12d1b176c26082e
Nicholas Davidson <nicholas.davidson.20@gmail.com>
2024-04-08 20:58:20 -0500

    Created Order Model.

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,6 +1,21 @@
-﻿namespace HHPW_BE.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HHPW_BE.Models
 {
     public class Order
     {
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        public string? Status { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+        public string? OrderType { get; set; }
+        public string? PaymentType { get; set; }
+        public int? Total { get; set; }
+        public int? Tip { get; set; }
+        public DateTime? Date { get; set; }
+        public int? UserId { get; set; }
+        public ICollection<OrderItem> Items { get; set; } = new List<OrderItem>();
     }
 }
