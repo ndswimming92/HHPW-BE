222b1ea33e7258982aa3cb2f67b37a737a26bf41
Nicholas Davidson <nicholas.davidson.20@gmail.com>
2024-04-08 20:28:30 -0500

    Created Item model.

--- Models/Item.cs
+++ Models/Item.cs
@@ -1,6 +1,13 @@
-﻿namespace HHPW_BE.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HHPW_BE.Models
 {
     public class Item
     {
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+        public ICollection<OrderItem> Orders { get; set; } = new List<OrderItem>();
     }
 }
