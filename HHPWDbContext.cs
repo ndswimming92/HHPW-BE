62dafb25b74da6f44abbe708e5ef73c761400881
Nicholas Davidson <nicholas.davidson.20@gmail.com>
2024-04-11 20:24:05 -0500

    updated DbContext File

--- HHPWDbContext.cs
+++ HHPWDbContext.cs
@@ -1,15 +1,26 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using HHPW_BE.Models;
+using HHPW_BE.Data;
 
 namespace HHPW_BE
 {
-    public class HHPWDbContext
+    public class HHPWDbContext : DbContext
     {
         public DbSet<User>? Users { get; set; }
         public DbSet<Item>? Items { get; set; }
         public DbSet<Order>? Orders { get; set; }
         public DbSet<OrderItem>? OrderItems { get; set; }
 
-      
+        public HHPWDbContext(DbContextOptions<HHPWDbContext> context) : base(context)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(UsersData.Users);
+            modelBuilder.Entity<Item>().HasData(ItemsData.Items);
+            modelBuilder.Entity<Order>().HasData(OrdersData.Orders);
+        }
+
     }
 }
